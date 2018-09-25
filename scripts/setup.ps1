6d48721de58f388593871812e550bb2ffc5ed685
Rolando Antón <ranton@fortinet.com>
2018-09-25 14:49:22 -0500

    Create setup.ps1

---
+++ scripts/setup.ps1
@@ -0,0 +1,26 @@
+# Supress network location Prompt
+New-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Network\NewNetworkWindowOff" -Force
+
+# Set network to private
+$ifaceinfo = Get-NetConnectionProfile
+Set-NetConnectionProfile -InterfaceIndex $ifaceinfo.InterfaceIndex -NetworkCategory Private 
+
+# Set up WinRM and configure some things
+
+winrm quickconfig -q
+winrm quickconfig -transport:http
+winrm set winrm/config '@{MaxTimeoutms="7200000"}'
+winrm set winrm/config/winrs '@{MaxMemoryPerShellMB="0"}'
+winrm set winrm/config/winrs '@{MaxProcessesPerShell="0"}'
+winrm set winrm/config/winrs '@{MaxShellsPerUser="0"}'
+winrm set winrm/config/service '@{AllowUnencrypted="true"}'
+winrm set winrm/config/service/auth '@{Basic="true"}'
+winrm set winrm/config/client/auth '@{Basic="true"}'
+winrm set winrm/config/listener?Address=*+Transport=HTTP '@{Port="5985"} '
+
+Enable-NetFirewallRule -DisplayName "Windows Remote Management (HTTP-In)"
+
+net stop winrm
+sc.exe config winrm start= auto
+net start winrm
+
